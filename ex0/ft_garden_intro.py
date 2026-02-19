f0e8df3df8891091be714262ce6704ffd3eeed31
roronan13 <ronan.pothier@gmail.com>
2026-02-19 14:32:26 +0100

    ok

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,13 @@
+def display() -> None:
+    plant = "chardon"
+    height = 1
+    age = 1
+    print(f'Plant : {plant}')
+    print(f'Height : {height}m')
+    print(f'Age : {age}y')
+
+
+if __name__ == "__main__":
+    print("-- Hello ! --")
+    display()
+    print("-- End of program. --")
