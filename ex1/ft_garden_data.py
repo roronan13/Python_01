1c3206c5fc1fee2fd3b50b689b1e4f56c389f1a5
roronan13 <ronan.pothier@gmail.com>
2026-02-19 16:24:40 +0100

    oui

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -1,5 +1,13 @@
 class Plant:
-    name = 
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
 
 
 if __name__ == "__main__":
+    print("-- My garden : --")
+    plant_1 = Plant("Rose", 100, 2)
+    plant_2 = Plant("Jasmin", 50, 6)
+    plant_3 = Plant("Lila", 200, 5)
+    
