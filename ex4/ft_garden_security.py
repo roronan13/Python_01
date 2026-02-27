1525cdd8ce61991256614545fffc1394da73c776
roronan13 <ronan.pothier@gmail.com>
2026-02-27 15:43:37 +0100

    ca soule

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -4,28 +4,34 @@ class Plant:
         self.__height = height
         self.__age = age
 
-    def set_height(self, height) -> None:
-        if height <= 200 and height >= 1:
+    def set_height(self, height) -> int:
+        if height <= 200 and height >= 0:
             self.__height = height
+            return 0
         else:
             print("This size can't be reached !")
+            return -1
 
     def get_height(self) -> int:
         return self.__height
 
-    def set_age(self, age) -> None:
-        if age <= 50 and age >= 1:
+    def set_age(self, age) -> int:
+        if age <= 50 and age >= 0:
             self.__age = age
+            return 0
         else:
             print("It can't be that age !")
+            return -1
 
     def get_age(self) -> int:
         return self.__age
 
+    def SecurePlant(self, height, age) -> None:
+        self.set_height(self, height)
+        self.set_age(self, age)
+
 
 if __name__ == "__main__":
-    plant1 = Plant("Rose", 10, 5)
-    plant2 = Plant("Tulipe", 20, 10)
-    plant3 = Plant("Jacinthe", 50, 20)
-    plant4 = Plant("Chrysalide", 80, 50)
-    plant5 = Plant("Begonia", 100, 300)
+    plant1 = Plant("Rose", 0, 0)
+    plant1.SecurePlant(int(input("Height : ")), int(input("Age : ")))
+    print(f'Plant created : {plant1.name}')
