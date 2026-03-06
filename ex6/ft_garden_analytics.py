3dc8d44a301b15ff956fd1cad884d275125b68e6
roronan13 <ronan.pothier@gmail.com>
2026-03-06 18:20:57 +0100

    je capte

--- ex6/ft_garden_analytics.py
+++ ex6/ft_garden_analytics.py
@@ -20,15 +20,31 @@ class PrizeFlower(FloweringPlant):
 class GardenManager:
     def __init__(self, name: str) -> None:
         self.owners = []
+        self.plants = []
         self.number_of_owners = 0
+        self.number_of_plants = 0
 
     def AddOwner(self, owner: str) -> None:
         self.owners.append(owner)
         self.number_of_owners += 1
+        self.owner_plants = []
 
-    def GetNumber(self) -> int:
+    def GetNumberOwners(self) -> int:
         return self.number_of_owners
 
+    def AddPlant(self, name: str, owner: str) -> None:
+        self.plants.append(name)
+        self.number_of_plants += 1
+        for self.owner in self.owners:
+            if self.owner == "owner":
+                self.owner_plants.append(name)
+
+
+class Owner:
+    def __init__(self, owner_name: str):
+        self.owner_name = owner_name
+        self.plants = []
+        self.number_of_plants = 0
 
     # def GardenStats(self) -> None:
 
@@ -40,6 +56,11 @@ if __name__ == "__main__":
     My_Garden = GardenManager("My Garden")
     My_Garden.AddOwner("Ronan")
     My_Garden.AddOwner("Jean")
+    My_Garden.AddOwner("Jacques")
     for My_Garden.owner in My_Garden.owners:
         print(f'{My_Garden.owner}')
-    print(f'Total gardens managed : {My_Garden.GetNumber()}')
+        for My_Garden.owner.name in My_Garden.owners:
+            print(f'  --  {My_Garden.owner.name}')
+    print(f'Total gardens managed : {My_Garden.GetNumberOwners()}')
+    My_Garden.AddPlant("Lila", "Ronan")
+
