468c8aa0c9409a225591c6b74b43d7dd02c4bafa
roronan13 <ronan.pothier@gmail.com>
2026-03-06 12:48:40 +0100

    ca soule

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -18,15 +18,40 @@ class Tree(Plant):
 
 class Flower(Plant):
     def __init__(self, name: str, height: int, age: int, color: int,
-                 bloomed: bool = false) -> None:
+                 bloomed: bool = False) -> None:
         super().__init__(name, height, age)
         self.color = color
 
     def bloom(self) -> bool:
         if self.age > 5:
-            self.bloomed = true
+            self.bloomed = True
+        else:
+            self.bloomed = False
+
+    def is_bloomed(self) -> None:
+        if self.bloomed:
+            print(f'{self.name} is blooming beautifully !')
+        else:
+            print(f'{self.name} is not blooming yet !')
 
 
 class Vegetable(Plant):
-    def __init__(self, name: str, height: int, age: int):
+    def __init__(self, name: str, height: int, age: int,
+                 harvest_season: str, nutritional_value: int) -> None:
         super().__init__(name, height, age)
+        self.harvest_season = harvest_season
+        self.nutritional_value = nutritional_value
+
+
+if __name__ == "__main__":
+    print(" -- Garden Plant Types --  ")
+    Lila = Flower("Lila", 10, 6, "purple")
+    Lila.bloom()
+    print(f'{Lila.name} (Flower) : {Lila.height}cm, {Lila.age} days, {Lila.color} color')
+    Lila.is_bloomed()
+    Rose = Flower("Rose", 20, 3, "rose")
+    Rose.bloom()
+    print(f'{Rose.name} (Flower) : {Rose.height}cm, {Rose.age} days, {Rose.color} color')
+    Rose.is_bloomed()
+    Oak = Tree("Oak", 3000, 50, 20)
+    
