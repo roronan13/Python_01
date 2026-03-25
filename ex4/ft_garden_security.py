335a6233325e209d61a83934433321b6ec64a7ec
roronan13 <ronan.pothier@gmail.com>
2026-03-25 17:15:55 +0100

    ok

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
+
 class Plant:
-    def __init__(self, name, height, age) -> None:
+    def __init__(self, name: str, height: int, age: int) -> None:
         self.name = name
         self.__height = height
         self.__age = age
 
-    def set_height(self, height) -> int:
+    def set_height(self, height: int) -> int:
         if height >= 0:
             self.__height = height
             return 0
@@ -12,10 +14,7 @@ class Plant:
             print("This size can't be reached !")
             return -1
 
-    def get_height(self) -> int:
-        return self.__height
-
-    def set_age(self, age) -> int:
+    def set_age(self, age: int) -> int:
         if age >= 0:
             self.__age = age
             return 0
@@ -23,31 +22,38 @@ class Plant:
             print("It can't be that age !")
             return -1
 
+    def get_height(self) -> int:
+        return self.__height
+
     def get_age(self) -> int:
         return self.__age
 
-    def SecurePlant(self, height, age) -> int:
+    def SecurePlant(self, height: int, age: int) -> int:
+        is_valid = 0
         if self.set_height(height) < 0:
             print("Security : wrong height !")
-            return -1
+            is_valid = -1
         if self.set_age(age) < 0:
             print("Security : wrong age !")
-            return -1
-        return 0
+            is_valid = -1
+        return is_valid
 
 
 if __name__ == "__main__":
+    print(" === Garden Security System === \n")
     plant1 = Plant("Rose", -1, -1)
+    print(f"{plant1.name} :")
     if plant1.SecurePlant(int(input("Height : ")), int(input("Age : "))) >= 0:
-        print(f'Plant created : {plant1.name}')
+        print(f'\nPlant created : {plant1.name}')
         print(f'Height updated : {plant1.get_height()}cm [OK]')
         print(f'Age updated : {plant1.get_age()}days [OK]')
     plant2 = Plant("Lila", -1, -1)
+    print(f"\n{plant2.name} :")
     if plant2.SecurePlant(int(input("Height : ")), int(input("Age : "))) >= 0:
-        print(f'Plant created : {plant2.name}')
+        print(f'\nPlant created : {plant2.name}')
         print(f'Height updated : {plant2.get_height()}cm [OK]')
         print(f'Age updated : {plant2.get_age()}days [OK]')
-    print("Current plants : ")
+    print("\nCurrent plants : ")
     if plant1.get_height() > -1 and plant1.get_age() > -1:
         print(f'{plant1.name} ({plant1.get_height()}cm, '
               f'{plant1.get_age()}days)')
