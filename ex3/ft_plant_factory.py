8967092d127edad50835c84b820d248930989758
roronan13 <ronan.pothier@gmail.com>
2026-03-25 16:49:55 +0100

    ok

--- ex3/ft_plant_factory.py
+++ ex3/ft_plant_factory.py
@@ -1,20 +1,25 @@
+#!/usr/bin/env python3
+
 class Plant:
-    def __init__(self, name, strt_h, strt_a) -> None:
+    def __init__(self, name: str, height: int, plant_age: int) -> None:
         self.name = name
-        self.strt_h = strt_h
-        self.strt_a = strt_a
+        self.height = height
+        self.plant_age = plant_age
+
+    def show(self) -> None:
+        print(f"Created: {self.name}: {self.height}cm, {self.plant_age} \
+days old")
 
 
 if __name__ == "__main__":
-    print(" -- Plant factory output -- ")
+    print(" -- Plant factory output -- \n")
     plant1 = Plant("Rose", 10, 5)
     plant2 = Plant("Tulipe", 20, 10)
     plant3 = Plant("Jacinthe", 50, 20)
     plant4 = Plant("Chrysalide", 80, 50)
     plant5 = Plant("Begonia", 100, 100)
-    print(f'Created : {plant1.name} ({plant1.strt_h}cm, {plant1.strt_a} days)')
-    print(f'Created : {plant2.name} ({plant2.strt_h}cm, {plant2.strt_a} days)')
-    print(f'Created : {plant3.name} ({plant3.strt_h}cm, {plant3.strt_a} days)')
-    print(f'Created : {plant4.name} ({plant4.strt_h}cm, {plant4.strt_a} days)')
-    print(f'Created : {plant5.name} ({plant5.strt_h}cm, {plant5.strt_a} days)')
-    print("Total plants created : 5")
+    plant1.show()
+    plant2.show()
+    plant3.show()
+    plant4.show()
+    plant5.show()
